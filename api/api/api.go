6a74b47b1a6cc995de8bc8d73d9f0803454a290f
Brian Lee <brianjleeofcl@gmail.com>
2022-10-10 16:56:26 -0700

    leave comments for ssr

--- api/api/api.go
+++ api/api/api.go
@@ -48,6 +48,10 @@ func serveFiles(router *chi.Mux) {
 	router.Get("/*", func(rw http.ResponseWriter, r *http.Request) {
 		interceptor := responseInterceptor{rw, 0}
 		http.FileServer(http.Dir("/api/frontend")).ServeHTTP(&interceptor, r)
+
+		// If requested file does not exist, serve frontend file
+		// This is also where Server rendered meta tag subsitution occurs when implemented
+		// for web crawler/link preview embeds
 		if interceptor.status == 404 {
 			rw.Header().Set("Content-Type", "text/html")
 			http.ServeFile(rw, r, "/api/frontend/index.html")
