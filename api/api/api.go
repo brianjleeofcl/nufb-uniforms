fc6a7bab63ec67a4799837e0e729149b8efa46fb
Brian Lee <brianjleeofcl@gmail.com>
2022-11-04 14:49:39 -0700

    api: reorg api package

--- api/api/api.go
+++ api/api/api.go
@@ -2,10 +2,8 @@ package api
 
 import (
 	"net/http"
-	"os"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func GetRouter() *chi.Mux {
@@ -14,36 +12,6 @@ func GetRouter() *chi.Mux {
 	serveFiles(router)
 	resetCache()
 	return router
-}
-
-func attachRoutes(router *chi.Mux) {
-	basicAuth := middleware.BasicAuth("restricted", getBasicAuth())
-	router.Route("/api", func(r chi.Router) {
-		r.Get("/data-summary", dataSummaryHandler)
-
-		r.Route("/game", func(r chi.Router) {
-			r.Get("/", latestGameHandler)
-			r.Get("/{season}/{week}", gameDetailHandler)
-			r.Get("/{season}/{week}/id", gameIDHandler)
-		})
-
-		r.Get("/games/{range}", gameSummaryHandler)
-
-		r.Route("/uniforms", func(r chi.Router) {
-			r.Get("/", uniformListHandler)
-			r.Get("/timeline", uniformTimelineHandler)
-		})
-
-		r.Get("/uniform/{helmet}-{jersey}-{pants}/{selection}", uniformDetailHanlder)
-
-		r.Route("/game-table", func(r chi.Router) {
-			r.Use(basicAuth)
-			r.Patch("/season", seasonUploadHandler)
-			r.Patch("/id/{id}", gameUploadHandler)
-		})
-
-		r.With(basicAuth).Put("/uniforms/new", uniformUploadHanlder)
-	})
 }
 
 func serveFiles(router *chi.Mux) {
@@ -59,10 +27,4 @@ func serveFiles(router *chi.Mux) {
 			http.ServeFile(rw, r, "/api/frontend/index.html")
 		}
 	})
-}
-
-func getBasicAuth() map[string]string {
-	return map[string]string{
-		os.Getenv("UPLOAD_USERNAME"): os.Getenv("UPLOAD_PASSWORD"),
-	}
 }
