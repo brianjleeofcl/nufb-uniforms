c4d79caa413b1ba355f1560f12ca2342f01bf23c
Brian Lee <brianjleeofcl@gmail.com>
2023-01-25 11:07:02 -0800

    DB: filter season length to data visible

--- data/migrate/migrations/04_additional-summary-data.up.sql
+++ data/migrate/migrations/04_additional-summary-data.up.sql
@@ -2,13 +2,13 @@ DROP MATERIALIZED VIEW data_summary;
 
 CREATE MATERIALIZED VIEW data_summary AS
 WITH seasons AS (
-    SELECT season, count(*) FROM full_data GROUP BY season),
+    SELECT season, count(*) FROM full_data WHERE show GROUP BY season),
 season_json AS (
     SELECT json_object_agg(season, count) AS lengths FROM seasons)
 SELECT json_build_object(
     'gameCount', count(*),
     'seasons', array_agg(DISTINCT season),
-    'seasonLengths', array_agg(lengths) FILTER(WHERE full_order = 1) ,
+    'seasonLengths', array_agg(lengths) FILTER (WHERE full_order = 1) ,
     'helmetColor', array_agg(DISTINCT helmet_color),
     'jerseyColor', array_agg(DISTINCT jersey_color),
     'pantsColor', array_agg(DISTINCT pants_color),
